586b0dd0d1ee9ebd77867864591a96e94f2e1be7
Ilaria Diliberto <ilaria.dil@gmail.com>
2025-12-01 11:45:58 +0100

    function add

--- lezione-5 quizzettone/quizzettone.py
+++ lezione-5 quizzettone/quizzettone.py
@@ -1,21 +1,28 @@
+"""
+mostra_domanda() (senza return)
+    * Non prende parametri
+    * Stampa la domanda e le 4 opzioni
+    * Non restituisce nulla
+
+* raccogli_risposta() (con return)
+    * Non prende parametri
+    * Chiede l'input all'utente
+    * Converte in int
+    * Restituisce il numero scelto
+
+* valida_scelta(scelta) (con return)
+    * prende come parametro il numero scelto
+    * Verifica se è tra 1 e 4 usando if
+    * Restituisce True se valida, False altrimenti
+"""
 
 
 
 
-
-
-
-def valida_scelta(scelta: str) -> bool:
-
-    scelta_tmp = scwelta.upper()
-    if scelta_tmp == "A" or scelta_tmp =="B" or scelta_tmp == "C" or scelta_tmp == "D":
-        return True
-    else:
-        return False
     
 def mostra_domanda() -> None:
+    """Stampa la domanda e le opzioni di risposta."""
     print( 
-        
         """
           
           Chi parteciperà a Sanremo 2026?
@@ -24,5 +31,22 @@ def mostra_domanda() -> None:
           B. La nina
           C. Nilla Pizzi
           D. Rocco Papaleo
-          
           """)
+
+
+def raccogli_risposta() -> str:
+        return input("Inserisci la tua scelta:)"
+                     
+
+def valida_scelta(scelta: str) -> bool:
+
+    scelta_tmp = scwelta.upper()
+    if scelta_tmp == "A" or scelta_tmp =="B" or scelta_tmp == "C" or scelta_tmp == "D":
+        return True
+    else:
+        return False
+
+mostra_domanda()
+
+risposta_da_validare: str = raccogli_risposta()
+risposta_validata: bool= valida_scelta(risposta_da_validare)
