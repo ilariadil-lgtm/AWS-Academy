86c91fedd681ead81638b56a8bf050be3ebd5a71
Ilaria Diliberto <ilaria.dil@gmail.com>
2025-12-01 11:50:14 +0100

    modifiche formattazione

--- lezione-5 quizzettone/quizzettone.py
+++ lezione-5 quizzettone/quizzettone.py
@@ -4,41 +4,38 @@ mostra_domanda() (senza return)
     * Stampa la domanda e le 4 opzioni
     * Non restituisce nulla
 
-* raccogli_risposta() (con return)
+raccogli_risposta() (con return)
     * Non prende parametri
     * Chiede l'input all'utente
     * Converte in int
-    * Restituisce il numero scelto
+    * Restituisce l'opzione scelta
 
-* valida_scelta(scelta) (con return)
+valida_scelta(scelta) (con return)
     * prende come parametro il numero scelto
-    * Verifica se è tra 1 e 4 usando if
+    * Verifica se è tra A,b,c d usando if
     * Restituisce True se valida, False altrimenti
 """
-
-
-
-
     
 def mostra_domanda() -> None:
     """Stampa la domanda e le opzioni di risposta."""
-    print( 
-        """
-          
+ 
+    print(  """
           Chi parteciperà a Sanremo 2026?
           
           A. Nayt
           B. La nina
           C. Nilla Pizzi
           D. Rocco Papaleo
+
           """)
 
-
 def raccogli_risposta() -> str:
+        """Chiede all'utente di inserire la proprio scelta e la restituisce."""
+        
         return input("Inserisci la tua scelta:)"
                      
-
 def valida_scelta(scelta: str) -> bool:
+        """ Verifica se la scelta è valida (A,B,C,D) e restituisce True o False."""
 
     scelta_tmp = scwelta.upper()
     if scelta_tmp == "A" or scelta_tmp =="B" or scelta_tmp == "C" or scelta_tmp == "D":
