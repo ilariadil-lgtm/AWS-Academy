a4014c268624b541a2171fba8bafca1fe84fc24d
Ilaria Diliberto <ilaria.dil@gmail.com>
2025-12-03 10:43:27 +0100

    add lezione g.8

--- lezione-5 quizzettone/quizzettone.py
+++ lezione-5 quizzettone/quizzettone.py
@@ -25,63 +25,76 @@ mostra_feedback(messaggio) (senza return)
 - Stampa il feedback in modo formattato
 - Non restituisce nulla
 """
-    
+
+import sys 
+
 def mostra_domanda(domanda: str) -> None:
     """Stampa la domanda e le opzioni di risposta."""
  
     print( domanda)
     
-def leggi_file() -> None:
-    with open("domande.txt", "r") as file:
-        content = file.read()
-        return content      
+def leggi_file(file_path: str) -> str:
+    """Legge il contenuto del file e lo restituisce come stringa."""
 
-def estrai_index(content: str) -> int:
+    with open(file_path, "r") as file:
+        content = file.read()
+        return content
+
+def estrai_index(content: str) -> int: 
+    """Restituisce l'indice del carattere speciale '£' che separa domanda e risposta."""
+
     return content.index("£")
 
 def estrai_domanda(content: str, index: int) -> str:
-    return  content[0:index]
-    
+    return content[0:index]
+
 def estrai_risposta(content: str, index: int) -> str:
     return content[index+1:]
 
 def raccogli_risposta() -> str:
         """Chiede all'utente di inserire la proprio scelta e la restituisce."""
+
         return input("Inserisci la tua scelta:")
 
 def valida_scelta(scelta: str) -> bool:
     """ Verifica se la scelta è valida (A,B,C,D) e restituisce True o False."""
-    scelta_tmp = scelta.upper()
-    if scelta_tmp == "A" or scelta_tmp =="B" or scelta_tmp == "C" or scelta_tmp == "D":
+
+    scelta_tmp: str = scelta.upper()
+    if scelta_tmp == "A" or scelta_tmp == "B" or scelta_tmp == "C" or scelta_tmp == "D":
         return True
-    else:
+    else: 
         return False
     
-def genera_feedback(scelta:str, is_risposta_corretta: bool) -> str:
-     """Restituisce il messaggio che indica all'utente se ha indovinato o meno. Questa funzione viene eseguita solo se la funzione di validazione restituisce True. """
-     if scelta.upper() == "A":
-          return "Hai indovinato!"
-     else:
-          return "Non hai indovinato, peccato ritenta"
+def genera_feedback(is_corretta: bool) -> str:
+    """Restituisce il messaggio che indica all'utente se ha indovinato o meno. 
+    Questa funzione viene eseguita solo se la funzione di validazione restituisce True. """
+
+    if is_corretta == True:
+        return "Hai indovinato!"
+    else:
+        return "Non hai indovinato. Ritenta!"
      
 def is_risposta_esatta(scelta: str, risposta_esatta: str) -> bool:
+    """Confronta la scelta dell'utente con la risposta esatta e restituisce True o False."""
+
     if scelta.upper() == risposta_esatta:
         return True
     else:
         return False
 
-is_risposta_corretta = False     
 def mostra_feedback(messaggio: str) -> None:
     """Stampa il messaggio di feedback in modo formattato."""
+
     simbol: str = "*"*30
     print(f"""
-          {simbol}
-          {messaggio}
-          {simbol}
-        """)
+{simbol}
+{messaggio}
+{simbol}
+""")
     
 def main():
-    content: str = leggi_file()
+    file_path: str = sys.argv[1]
+    content: str = leggi_file(file_path)
     index: int = estrai_index(content)
     domanda: str = estrai_domanda(content, index)
     risposta: str = estrai_risposta(content, index)
@@ -94,9 +107,8 @@ def main():
         feedback: str = ""
 
         if risposta_validata == True:
-            print(f"sono qui: {risposta}")
             is_risposta_corretta = is_risposta_esatta(risposta_da_validare, risposta)
-            feedback = genera_feedback(risposta_da_validare, is_risposta_corretta)
+            feedback = genera_feedback(is_risposta_corretta)
         else: 
             feedback = "Inserisci solo la risposta tra le opzioni elencate"
 
