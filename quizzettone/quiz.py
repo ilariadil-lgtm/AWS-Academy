7fab3058e27adf411aa3f92d9858ec55a489bd55
Ilaria Diliberto <ilaria.dil@gmail.com>
2025-12-09 11:36:56 +0100

    feat: add function

---
+++ quizzettone/quiz.py
@@ -0,0 +1,28 @@
+"""
+mostra_domanda() (senza return)
+- Non prende parametri
+- Stampa la domanda e le 4 opzioni
+- Non restituisce nulla
+
+raccogli_risposta() (con return)
+- Non prende parametri
+- Chiede l'input all'utente
+- Converte in int
+- Restituisce l'opzione scelta
+
+valida_scelta(scelta) (con return)
+- prende come parametro il numero scelto
+- Verifica se è tra A,b,c d usando if
+- Restituisce True se valida, False altrimenti
+
+genera_feedback(scelta) (con return)
+- Prende come parametro la lettera che è stata scelta 
+- Usa if/elif/else per determinare il messaggio
+- Restituisce la stringa con il feedback personalizzato
+
+mostra_feedback(messaggio) (senza return)
+- Prende come parametro una stringa
+- Stampa il feedback in modo formattato
+- Non restituisce nulla
+"""
+
